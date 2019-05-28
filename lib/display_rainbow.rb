57be5011be36af02a3633b5fcbd8ce1508225f3f
observant-communicator-1765 <timothyjshen@gmail.com>
2019-05-28 19:12:58 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
-def display_rainbow(r,o,y,g,i,v)
+def display_rainbow("r","o","y","g","i","v")
   puts "R: #{r}, O: #{o}, Y: #{y}, G: #{g}, I: #{i}, V: #{v}"
 end
