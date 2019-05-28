192f8ed61287acf3b6a0828f1ce80d12e8178956
observant-communicator-1765 <timothyjshen@gmail.com>
2019-05-28 19:41:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,9 +4,3 @@
 def display_rainbow(a)
   puts "R: #{a[0]}, O: #{a[1]}, Y: #{a[2]}, G: #{a[3]}, B: #{a[4]}, I: #{a[5]}, V: #{a[6]}"
 end
-
-
-def display_rainbow(r,o,y,g,b,i,v)
-end
-
-display_rainbow(['r', 'o', 'y', 'g', 'b', 'i', 'v'])
